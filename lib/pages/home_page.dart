f6ff6907dcb4b26b071347fd0f6f28d51aa6965a
Osama Nabil <121755701+01121922383osama@users.noreply.github.com>
2024-08-05 23:27:10 +0300

    add fake comment

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
+// by osama
   @override
   Widget build(BuildContext context) {
     return Scaffold(
